799b2f75f0d24299f80da96f132b03269aa1387d
nhk2204 <genome1_31@yahoo.co.jp>
2016-10-02 00:41:29 +0900

    間違い探し その4

--- app/src/main/java/jp/techacademy/kanta/nakayama/taskapp/InputActivity.java
+++ app/src/main/java/jp/techacademy/kanta/nakayama/taskapp/InputActivity.java
@@ -1,6 +1,8 @@
 package jp.techacademy.kanta.nakayama.taskapp;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -151,5 +153,16 @@ public class InputActivity extends AppCompatActivity {
         realm.commitTransaction();
 
         realm.close();
+
+        Intent resultIntent=new Intent(getApplicationContext(),TaskAlarmReceiver.class);
+        resultIntent.putExtra(MainActivity.EXTRA_TASK,mTask);
+        PendingIntent resultPendingIntent=PendingIntent.getBroadcast(
+                this,
+                mTask.getId(),
+                resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        AlarmManager alarmManager=(AlarmManager)getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),resultPendingIntent);
     }
 }
